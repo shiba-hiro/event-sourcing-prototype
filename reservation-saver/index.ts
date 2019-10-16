5429699975913d2bde864de191748f37141324ba
Hirofumi Shibata <33661448+shiba-hiro@users.noreply.github.com>
2019-10-17 01:32:00 +0900

    Store in firestore

--- reservation-saver/index.ts
+++ reservation-saver/index.ts
@@ -1,21 +1,41 @@
-// import { Firestore } from '@google-cloud/firestore';
+import { Firestore } from '@google-cloud/firestore';
 
-// const db = new Firestore({
-//   projectId: process.env.PROJECT_ID,
-//   keyFilename: process.env.KEY_FILENAME,
-// });
+const db = new Firestore({
+  projectId: process.env.PROJECT_ID,
+  keyFilename: process.env.KEY_FILENAME,
+});
 
-const storeSubmittedReservation = (event: Record<string, any>): void => {
+interface EventParameter {
+  reservationId: {stringValue: string};
+  from: {stringValue: string};
+  to: {stringValue: string};
+  unit: {integerValue: string};
+  timestamp: {integerValue: string};
+}
+
+const storeSubmittedReservation = async (event: Record<string, any>) => {
+  const params = JSON.parse(Buffer.from(event.data, 'base64').toString()) as EventParameter;
+
   try {
-    const params = JSON.parse(Buffer.from(event.data, 'base64').toString());
-    console.log(params);
-    console.log(params.from);
-    console.log(params.to);
-    console.log(params.unit);
-    console.log(params.reservationId);
+    await db
+      .collection('reservations')
+      .doc(params.reservationId.stringValue)
+      .set({
+        from: params.from.stringValue,
+        to: params.to.stringValue,
+        unit: Number(params.unit.integerValue),
+        createdAt: Number(params.timestamp.integerValue),
+        status: 'isSubmitting',
+        creationValidations: {
+          reservation: 'passed',
+        },
+      });
   } catch (error) {
+    console.error(`Failed to store the reservation: ${params.reservationId.stringValue}`);
     console.error(error);
+    throw error;
   }
+  console.log(`Successfully stored the reservation: ${params.reservationId.stringValue}`);
 };
 
 export {
